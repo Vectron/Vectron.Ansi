d219e110e77d8ceed98f83ad60e7e22d7f7a2486
Vectron <thijs_bloebaum@hotmail.com>
2023-08-30 15:48:42 +0200

    Add byte colors to textwriter extensions

---
+++ src/Vectron.Ansi/TextWriterExtensions.ByteColor.cs
@@ -0,0 +1,111 @@
+namespace Vectron.Ansi;
+
+/// <summary>
+/// Extension methods for writing Ansi escape sequences to a <see cref="TextWriter"/>.
+/// </summary>
+public static partial class TextWriterExtensions
+{
+    /// <summary>
+    /// Write a colored message and reset the color at the end.
+    /// </summary>
+    /// <param name="textWriter">The <see cref="TextWriter"/> to use.</param>
+    /// <param name="text">The text to write.</param>
+    /// <param name="color">The <see cref="byte"/>.</param>
+    public static void WriteColored(this TextWriter textWriter, string text, byte color)
+    {
+        var escapeCode = AnsiHelper.GetAnsiEscapeCode(color);
+        textWriter.WriteCodeAndReset(text, escapeCode);
+    }
+
+    /// <summary>
+    /// Write a colored message and reset the color at the end.
+    /// </summary>
+    /// <param name="textWriter">The <see cref="TextWriter"/> to use.</param>
+    /// <param name="text">The text to write.</param>
+    /// <param name="color">The <see cref="byte"/>.</param>
+    /// <param name="style">The text style.</param>
+    public static void WriteColored(this TextWriter textWriter, string text, byte color, AnsiStyle style)
+    {
+        var escapeCode = AnsiHelper.GetAnsiEscapeCode(color, background: false, style);
+        textWriter.WriteCodeAndReset(text, escapeCode);
+    }
+
+    /// <summary>
+    /// Write a colored message and reset the color at the end.
+    /// </summary>
+    /// <param name="textWriter">The <see cref="TextWriter"/> to use.</param>
+    /// <param name="text">The text to write.</param>
+    /// <param name="foregroundColor">The foreground <see cref="byte"/> to use.</param>
+    /// <param name="backgroundColor">The background <see cref="byte"/> to use.</param>
+    public static void WriteColored(this TextWriter textWriter, string text, byte foregroundColor, byte backgroundColor)
+    {
+        var escapeCode = AnsiHelper.GetAnsiEscapeCode(foregroundColor, backgroundColor);
+        textWriter.WriteCodeAndReset(text, escapeCode);
+    }
+
+    /// <summary>
+    /// Write a colored message and reset the color at the end.
+    /// </summary>
+    /// <param name="textWriter">The <see cref="TextWriter"/> to use.</param>
+    /// <param name="text">The text to write.</param>
+    /// <param name="foregroundColor">The foreground <see cref="byte"/> to use.</param>
+    /// <param name="backgroundColor">The background <see cref="byte"/> to use.</param>
+    /// <param name="style">The text style.</param>
+    public static void WriteColored(this TextWriter textWriter, string text, byte foregroundColor, byte backgroundColor, AnsiStyle style)
+    {
+        var escapeCode = AnsiHelper.GetAnsiEscapeCode(foregroundColor, backgroundColor, style);
+        textWriter.WriteCodeAndReset(text, escapeCode);
+    }
+
+    /// <summary>
+    /// Write a colored message and reset the color at the end.
+    /// </summary>
+    /// <param name="textWriter">The <see cref="TextWriter"/> to use.</param>
+    /// <param name="text">The text to write.</param>
+    /// <param name="color">The <see cref="byte"/>.</param>
+    public static void WriteColored(this TextWriter textWriter, Span<char> text, byte color)
+    {
+        var escapeCode = AnsiHelper.GetAnsiEscapeCode(color);
+        textWriter.WriteCodeAndReset(text, escapeCode);
+    }
+
+    /// <summary>
+    /// Write a colored message and reset the color at the end.
+    /// </summary>
+    /// <param name="textWriter">The <see cref="TextWriter"/> to use.</param>
+    /// <param name="text">The text to write.</param>
+    /// <param name="color">The <see cref="byte"/>.</param>
+    /// <param name="style">The text style.</param>
+    public static void WriteColored(this TextWriter textWriter, Span<char> text, byte color, AnsiStyle style)
+    {
+        var escapeCode = AnsiHelper.GetAnsiEscapeCode(color, background: false, style);
+        textWriter.WriteCodeAndReset(text, escapeCode);
+    }
+
+    /// <summary>
+    /// Write a colored message and reset the color at the end.
+    /// </summary>
+    /// <param name="textWriter">The <see cref="TextWriter"/> to use.</param>
+    /// <param name="text">The text to write.</param>
+    /// <param name="foregroundColor">The foreground <see cref="byte"/> to use.</param>
+    /// <param name="backgroundColor">The background <see cref="byte"/> to use.</param>
+    public static void WriteColored(this TextWriter textWriter, Span<char> text, byte foregroundColor, byte backgroundColor)
+    {
+        var escapeCode = AnsiHelper.GetAnsiEscapeCode(foregroundColor, backgroundColor);
+        textWriter.WriteCodeAndReset(text, escapeCode);
+    }
+
+    /// <summary>
+    /// Write a colored message and reset the color at the end.
+    /// </summary>
+    /// <param name="textWriter">The <see cref="TextWriter"/> to use.</param>
+    /// <param name="text">The text to write.</param>
+    /// <param name="foregroundColor">The foreground <see cref="byte"/> to use.</param>
+    /// <param name="backgroundColor">The background <see cref="byte"/> to use.</param>
+    /// <param name="style">The text style.</param>
+    public static void WriteColored(this TextWriter textWriter, Span<char> text, byte foregroundColor, byte backgroundColor, AnsiStyle style)
+    {
+        var escapeCode = AnsiHelper.GetAnsiEscapeCode(foregroundColor, backgroundColor, style);
+        textWriter.WriteCodeAndReset(text, escapeCode);
+    }
+}
