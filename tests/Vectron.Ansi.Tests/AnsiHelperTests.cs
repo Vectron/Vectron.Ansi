f5457141dc747c3fe0d9ce72835ed4d430b7668e
Vectron <thijs_bloebaum@hotmail.com>
2023-08-30 15:15:09 +0200

    Add RGB color code generation

--- tests/Vectron.Ansi.Tests/AnsiHelperTests.cs
+++ tests/Vectron.Ansi.Tests/AnsiHelperTests.cs
@@ -310,6 +310,72 @@ public class AnsiHelperTests
     }
 
     [TestMethod]
+    [DataRow((byte)15, (byte)100, (byte)200, AnsiStyle.Italic | AnsiStyle.Blinking, "\x1b[38;2;15;100;200m\x1b[3m\x1b[5m", DisplayName = "Foreground")]
+    public void GetAnsiEscapeCodeReturnsProperRGBColorAndAndStyleCode(byte red, byte green, byte blue, AnsiStyle style, string expected)
+    {
+        // Arrange
+        var color = System.Drawing.Color.FromArgb(red, green, blue);
+
+        // Act
+        var code1 = AnsiHelper.GetAnsiEscapeCode(color, style);
+        var code2 = AnsiHelper.GetAnsiEscapeCode(red, green, blue, style);
+
+        // Assert
+        Assert.AreEqual(expected, code1);
+        Assert.AreEqual(expected, code2);
+    }
+
+    [TestMethod]
+    [DataRow((byte)15, (byte)100, (byte)200, false, AnsiStyle.Italic | AnsiStyle.Blinking, "\x1b[38;2;15;100;200m\x1b[3m\x1b[5m", DisplayName = "Foreground")]
+    [DataRow((byte)15, (byte)100, (byte)200, true, AnsiStyle.Italic | AnsiStyle.Blinking, "\x1b[48;2;15;100;200m\x1b[3m\x1b[5m", DisplayName = "Background")]
+    public void GetAnsiEscapeCodeReturnsProperRGBColorAndBackgroundAndStyleCode(byte red, byte green, byte blue, bool background, AnsiStyle style, string expected)
+    {
+        // Arrange
+        var color = System.Drawing.Color.FromArgb(red, green, blue);
+
+        // Act
+        var code1 = AnsiHelper.GetAnsiEscapeCode(color, background, style);
+        var code2 = AnsiHelper.GetAnsiEscapeCode(red, green, blue, background, style);
+
+        // Assert
+        Assert.AreEqual(expected, code1);
+        Assert.AreEqual(expected, code2);
+    }
+
+    [TestMethod]
+    [DataRow((byte)15, (byte)100, (byte)200, false, "\x1b[38;2;15;100;200m", DisplayName = "Foreground")]
+    [DataRow((byte)15, (byte)100, (byte)200, true, "\x1b[48;2;15;100;200m", DisplayName = "Background")]
+    public void GetAnsiEscapeCodeReturnsProperRGBColorAndBackgroundCode(byte red, byte green, byte blue, bool background, string expected)
+    {
+        // Arrange
+        var color = System.Drawing.Color.FromArgb(red, green, blue);
+
+        // Act
+        var code1 = AnsiHelper.GetAnsiEscapeCode(color, background);
+        var code2 = AnsiHelper.GetAnsiEscapeCode(red, green, blue, background);
+
+        // Assert
+        Assert.AreEqual(expected, code1);
+        Assert.AreEqual(expected, code2);
+    }
+
+    [TestMethod]
+    [DataRow((byte)15, (byte)100, (byte)200, "\x1b[38;2;15;100;200m", DisplayName = "Foreground")]
+    public void GetAnsiEscapeCodeReturnsProperRGBColorAndCode(byte red, byte green, byte blue, string expected)
+    {
+        // Arrange
+        var color = System.Drawing.Color.FromArgb(red, green, blue);
+
+        // Act
+        var code1 = AnsiHelper.GetAnsiEscapeCode(color);
+        var code2 = AnsiHelper.GetAnsiEscapeCode(red, green, blue);
+
+        // Assert
+        Assert.AreEqual(expected, code1);
+        Assert.AreEqual(expected, code2);
+    }
+
+    [TestMethod]
     [DataRow(AnsiStyle.None, "", DisplayName = "None")]
     [DataRow(AnsiStyle.Bold, "\x1b[1m", DisplayName = "Bold")]
     [DataRow(AnsiStyle.DimFaint, "\x1b[2m", DisplayName = "DimFaint")]
