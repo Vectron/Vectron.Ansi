7955504b6115a69713384d3cc5da4985dd9dca46
Vectron <thijs_bloebaum@hotmail.com>
2023-08-30 14:44:26 +0200

    Fix cursor code matching regex

--- src/Vectron.Ansi/AnsiHelper.Regex.cs
+++ src/Vectron.Ansi/AnsiHelper.Regex.cs
@@ -13,7 +13,7 @@ internal static partial class AnsiHelper
         TimeSpan.FromSeconds(1));
 
     private static readonly System.Text.RegularExpressions.Regex MatchCursorEscapeSequenceRegex = new(
-            @"\x1B\[[^@-~]*[ABCDabcd]",
+            @"\x1B\[[0-9;]*[A-Ga-g]",
             System.Text.RegularExpressions.RegexOptions.Compiled,
             TimeSpan.FromSeconds(1));
 
@@ -35,7 +35,7 @@ internal static partial class AnsiHelper
     /// </summary>
     /// <returns>The compiled regex for matching.</returns>
     [System.Text.RegularExpressions.GeneratedRegex(
-        pattern: @"\x1B\[[^@-~]*[ABCDabcd]",
+        pattern: @"\x1B\[[0-9;]*[A-Ga-g]",
         options: System.Text.RegularExpressions.RegexOptions.None,
         matchTimeoutMilliseconds: 1000)]
     public static partial System.Text.RegularExpressions.Regex MatchCursorEscapeSequence();
