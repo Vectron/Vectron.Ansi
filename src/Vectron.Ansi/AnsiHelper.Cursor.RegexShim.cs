340f5b7cc59043b0f6d5ecd5e46eaa971ce124cc
Vectron <thijs_bloebaum@hotmail.com>
2025-12-15 14:25:16 +0100

    Remove unused #if

--- src/Vectron.Ansi/AnsiHelper.Cursor.RegexShim.cs
+++ src/Vectron.Ansi/AnsiHelper.Cursor.RegexShim.cs
@@ -5,7 +5,6 @@ namespace Vectron.Ansi;
 /// </summary>
 public static partial class AnsiHelper
 {
-#if NET7_0_OR_GREATER
     /// <summary>
     /// Find al ANSI cursor escape codes.
     /// </summary>
@@ -15,19 +14,4 @@ public static partial class AnsiHelper
         options: System.Text.RegularExpressions.RegexOptions.None,
         matchTimeoutMilliseconds: 1000)]
     public static partial System.Text.RegularExpressions.Regex MatchCursorEscapeSequence();
-#else
-
-    private static readonly System.Text.RegularExpressions.Regex MatchCursorEscapeSequenceRegex = new(
-        @"\x1B\[[0-9;]*[A-Ga-g]",
-        System.Text.RegularExpressions.RegexOptions.Compiled,
-        TimeSpan.FromSeconds(1));
-
-    /// <summary>
-    /// Find al ANSI cursor escape codes.
-    /// </summary>
-    /// <returns>The compiled regex for matching.</returns>
-    public static System.Text.RegularExpressions.Regex MatchCursorEscapeSequence()
-        => MatchCursorEscapeSequenceRegex;
-
-#endif
 }
